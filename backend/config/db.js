c97df75e1f0a2b28bff4df36ce082ba5d1804a4a
teumaywerashe <teumay21@gmail.com>
2025-12-26 16:25:55 +0300

    fix: update PrismaClient initialization for cloud deployment and improve code formatting in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,18 @@
 // backend/db.js
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma =
+  //     new PrismaClient();
+  // import { PrismaClient } from "@prisma/client";
+
+  // export default
+  new PrismaClient({
+    // Add this for cloud/deployment to avoid connection issues
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 
 export default prisma;
